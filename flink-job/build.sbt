5a780fa0bd6f38f3821a7fa57f393046751d867d
Kathryn Lovell <krlovell017@gmail.com>
2025-09-21 21:49:10 -0400

    flink job

--- flink-job/build.sbt
+++ flink-job/build.sbt
@@ -10,21 +10,21 @@ lazy val root = (project in file("."))
     // Entry point for Flink
     Compile / mainClass := Some("com.example.ErrorPatternJob"),
 
-    libraryDependencies ++= Seq(
-      // Flink core Scala API modules
-      "org.apache.flink" %% "flink-streaming-scala" % "1.19.0" % Provided,
-      "org.apache.flink" %% "flink-scala"           % "1.19.0" % Provided,
+libraryDependencies ++= Seq(
+  // Flink core Scala API modules
+  "org.apache.flink" %% "flink-streaming-scala" % "1.19.0" % Provided,
+  "org.apache.flink" %% "flink-scala"           % "1.19.0" % Provided,
 
-      // Flink clients (Java-only, no Scala suffix!)
-      "org.apache.flink" %  "flink-clients"         % "1.19.0" % Provided,
+  // Flink clients (Java-only, no Scala suffix!)
+  "org.apache.flink" %  "flink-clients"         % "1.19.0" % Provided,
 
-      // Kafka connector (new unified connector)
-      "org.apache.flink" %  "flink-connector-kafka" % "3.3.0-1.19",
+  // Kafka connector (needs to be bundled!)
+  "org.apache.flink" %  "flink-connector-kafka" % "3.3.0-1.19",
 
-      // CEP (Complex Event Processing)
-      "org.apache.flink" %% "flink-cep-scala"       % "1.19.0" % Provided,
-      "org.apache.flink" %  "flink-cep"             % "1.19.0" % Provided
-    ),
+  // CEP (must be bundled!)
+  "org.apache.flink" %% "flink-cep-scala"       % "1.19.0",
+  "org.apache.flink" %  "flink-cep"             % "1.19.0"
+),
 
     // Merge strategy for shading
     assembly / assemblyMergeStrategy := {
--- flink-job/src/main/scala/com/example/ErrorPatternJob.scala
+++ flink-job/src/main/scala/com/example/ErrorPatternJob.scala
@@ -1,15 +1,12 @@
 package com.example
+
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.cep.scala.CEP
-import org.apache.flink.cep.scala.pattern.Pattern
-import org.apache.flink.cep.PatternSelectFunction
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer
-import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.eventtime.WatermarkStrategy
 
 import java.util.Properties
-
 
 object ErrorPatternJob {
   def main(args: Array[String]): Unit = {
@@ -20,32 +17,28 @@ object ErrorPatternJob {
     props.setProperty("group.id", "flink-error-detector")
 
     // Kafka consumer for "events"
-    val consumer = new FlinkKafkaConsumer[String]("events", new org.apache.flink.api.common.serialization.SimpleStringSchema(), props)
-    val stream = env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
-    stream.print("DEBUG-INCOMING")
-    // Define CEP pattern: 5 ERROR messages within 5 minutes
-    val pattern = Pattern
-      .begin[String]("any")
-      .times(2)
-      .within(Time.seconds(10))
-
-    val patternStream = CEP.pattern(stream, pattern)
-
-    val alerts = patternStream.select(new PatternSelectFunction[String, String] {
-      override def select(map: java.util.Map[String, java.util.List[String]]): String = {
-        "High error rate detected in the last 5 minutes"
-      }
-    })
-
-    // Send alerts to Kafka "alerts" topic
-    val producer = new FlinkKafkaProducer[String](
-      "alerts",
-      new org.apache.flink.api.common.serialization.SimpleStringSchema(),
+    val consumer = new FlinkKafkaConsumer[String](
+      "events",
+      new SimpleStringSchema(),
       props
     )
-    alerts.print("DEBUG-ALERTS")
-    alerts.addSink(producer)
 
-    env.execute("Error Pattern Detection Job")
+    val stream = env
+      .addSource(consumer)
+      .assignTimestampsAndWatermarks(WatermarkStrategy.noWatermarks[String]())
+
+    // Debug incoming events
+    stream.print("DEBUG-INCOMING")
+
+    // Just forward to "alerts" topic
+    val producer = new FlinkKafkaProducer[String](
+      "alerts",
+      new SimpleStringSchema(),
+      props
+    )
+
+    stream.addSink(producer)
+
+    env.execute("Event Passthrough Job")
   }
 }
